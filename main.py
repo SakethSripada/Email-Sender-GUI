67a7596296276ceb1cc073f385d15a94e7acb06a
Saketh Sripada <sakeats@gmail.com>
2023-11-29 23:13:41 -0600

    added ability to send multiple emails at once

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class EmailSender:
         self.user_email = ttk.Entry(self.root, font=("Arial", 18))
         self.user_email.pack()
 
-        self.pass_label = ttk.Label(self.root, text="Enter Your Mail Password", font=("Arial", 10))
+        self.pass_label = ttk.Label(self.root, text="Enter Your App Password", font=("Arial", 10))
         self.pass_label.pack()
         self.user_pass = ttk.Entry(self.root, font=("Arial", 18))
         self.user_pass.pack()
@@ -40,7 +40,7 @@ class EmailSender:
 
         self.number_emails_label = ttk.Label(self.root, text="How many Emails do you want to send?", font=("Arial", 13))
         self.number_emails_label.pack()
-        self.number_of_emails = ttk.Entry(self.root,  font=("Arial", 14))
+        self.number_of_emails = ttk.Entry(self.root, font=("Arial", 14))
         self.number_of_emails.pack()
 
         self.whitespace2 = ttk.Label(self.root, text="")
@@ -51,23 +51,28 @@ class EmailSender:
         self.root.mainloop()
 
     def sort_email_list(self):
-            email_text = self.email_list.get("1.0", "end").strip()
-            emails = [email.strip() for email in email_text.split(",")]
-            sorted_emails = list(set(filter(None, emails)))
-            return sorted_emails
+        email_text = self.email_list.get("1.0", "end").strip()
+        emails = [email.strip() for email in email_text.split(",")]
+        sorted_emails = list(set(filter(None, emails)))
+        return sorted_emails
 
     def send_emails(self):
-            useremail = self.user_email.get()
-            userpass = self.user_pass.get()
-            recipients = self.sort_email_list()
-            email_content = self.email_content.get("1.0", "end")
+        useremail = self.user_email.get()
+        userpass = self.user_pass.get()
+        recipients = self.sort_email_list()
+        email_content = self.email_content.get("1.0", "end")
 
-            server = smtplib.SMTP("smtp.gmail.com", 587)
-            server.starttls()
-            server.login(useremail, userpass)
+        try:
+            number_of_emails = int(self.number_of_emails.get())
+        except ValueError:
+            messagebox.showerror("Please enter a valid number of emails")
+
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(useremail, userpass)
+        for i in range(number_of_emails):
             for recipient in recipients:
                 server.sendmail(useremail, recipient, email_content)
 
 
 EmailSender()
-
